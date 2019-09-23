4f0305df1267ca5408fa68e5e373edb1e7697100
happiness <happinesskawinzi@gmail.com>
2019-09-23 10:06:36 +0300

    creating bootstrap instance,app configuration,registering blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,17 @@ from flask_simplemde import SimpleMDE
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 Login_manager = LoginManager()
+Login_manager.session_protection = 'strong'
+Login_manager.login_view = 'auth.login'
+mail = Mail()
+simple = SimpleMDE()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
