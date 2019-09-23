137c4f1c17b2c22ed5ca5da3cb4234e84509692e
happiness <happinesskawinzi@gmail.com>
2019-09-23 08:44:05 +0300

    creating development anf production classes

--- config.py
+++ config.py
@@ -7,3 +7,16 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hapiness:chapati@localhost:5432/kawi'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
