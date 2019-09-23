b1c16d6899ede67029b419f51078401c220f95db
happiness <happinesskawinzi@gmail.com>
2019-09-23 10:13:04 +0300

    initializing flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -22,3 +22,19 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    from .requests import configure_request
+    configure_request(app)
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    Login_manager.init_app(app)
+    simple.init_app(app)
+    mail.init_app(app)
+    configure_uploads(app,photos)
+
+    return app
+
+
