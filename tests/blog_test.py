93b93ffb8f3c151f7d92a84babc6e6d39f313cc6
happiness <happinesskawinzi@gmail.com>
2019-09-23 14:52:54 +0300

    adding unittest that will confirm if a blog is saved

---
+++ tests/blog_test.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models import User,Blog
+from app import db
+
+class TestBlog(unittest.TestCase):
+    def setUp(self):
+        self.user_happiness = User(firstname = 'Happiness',password = 'chapati')
+        self.new_blog = Blog(id=12345,text='vegetable',user_id=1,category='nutrition',user = self.user_happiness)
+
+    def tearDown(self):
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_blog,Blog))
+        def test_check_instance_variables(self):
+        self.assertEquals(self.new_blog.id,12345)
+        self.assertEquals(self.new_blog.user_id,1)
+        self.assertEquals(self.new_blog.category,'nutrition')
+        self.assertEquals(self.new_blog.user,self.user_happiness)
+
+
+    def test_save_post(self):
+        self.new_blog.save_blog()
+        blogs = Blog.query.all()
+        self.assertTrue(len(blogs)>0)
