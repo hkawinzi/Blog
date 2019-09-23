aa8e0d7fbaced2915619eb843ab76fd5e7868270
happiness <happinesskawinzi@gmail.com>
2019-09-23 08:58:39 +0300

    creating manager and migrate instances

--- manage.py
+++ manage.py
@@ -2,3 +2,8 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
