2661f00229ca8c0325e11e5bd1a0884415e289be
happiness <happinesskawinzi@gmail.com>
2019-09-23 12:34:00 +0300

    initializing blueprint in the main folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,9 +12,9 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next')) or url_for('main.profile',uname=current_user.username))
+            return redirect(request.args.get('next')) or url_for('main.profile',uname=current_user.firstname))
 
-        flash('Invalid username or Password')
+        flash('Invalid firstname or Password')
 
     title = "log in"
     return render_template('auth/login.html',login_form = login_form,title=title)
@@ -29,7 +29,7 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = from.email.data, firstname = form.username.data,password = form.password.data)
+        user = User(email = from.email.data, firstname = form.firstname.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
